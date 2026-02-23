932bdf3d99e73e07b24113a1b483534209c2a2c2
ekta-dew25 <ekta.dewangan1995@gmail.com>
2026-02-23 16:22:22 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,73 @@
+// const express = require('express');
+// const apiRouter = require('./routes/api');
+
+// const app = express();
+// const port = process.env.PORT || 3000;
+
+// app.use(express.json());
+
+// // Mount API router under /api
+// app.use('/api', apiRouter);
+
+// // Root route
+// app.get('/', (req, res) => {
+//   res.send('Hello from myPractice Express server!');
+// });
+
+// // 404 handler
+// app.use((req, res) => {
+//   res.status(404).json({ status: 'error', message: 'Not Found' });
+// });
+
+// // Error handler
+
+// app.listen(port, () => {
+//   console.log(`Server running at http://localhost:${port}`);
+// });
+
+const express = require('express')
+
+const app = express()
+
+const dummyData = [
+  { id: 1, name: 'Alice' },
+  { id: 2, name: 'Bob' },
+  { id: 3, name: 'Charlie' }
+]
+
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+ res.json("welcome to myPractice API")})
+
+app.get('/users',(req,res)=>{
+ res.json({
+  message:"User list retrieved successfully",
+  data:dummyData
+ })
+})
+
+app.post('/users',(req,res)=>{
+  const { name } = req.body
+
+  const newUser = {
+    id: dummyData.length+1,
+    name
+  }
+
+  dummyData.push(newUser)
+
+  res.status(201).json({
+    message:"user created successfully",
+    data:newUser
+  })
+
+})
+
+
+
+const PORT = 3000
+
+app.listen(PORT,()=>{
+  console.log(`Server running at http://localhost:${PORT}`)
+})
